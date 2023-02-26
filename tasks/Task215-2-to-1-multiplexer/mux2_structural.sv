d8a95acf00152a539b74af5ff581abdeb386153f
SayeedRahman5 <sayeed.rahman@students.plymouth.ac.uk>
2023-02-26 10:03:18 +0000

    Task 215 Fully complete
    Tested mux2 structural with the testbench

--- tasks/Task215-2-to-1-multiplexer/mux2_structural.sv
+++ tasks/Task215-2-to-1-multiplexer/mux2_structural.sv
@@ -1,6 +1,15 @@
 module mux2_structural (output logic y, input logic a, b, s);
 
 // Write HDL here
+//Internal wires
+wire m1,m2,not_s;
+
+//Instantiate and connect logic gates
+not u1(not_s, s);
+and u2(m1, not_s, a);
+and u3(m2, s, b);
+or  u4(y, m1, m2);
+
 
 
 endmodule
